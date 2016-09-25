be6624f898a7ed3f62dbde60ac63b7d4e1610c10
MISAEL DIFO CONTRERA <misaedifo@hotmail.com>
2016-09-25 17:24:03 -0400

    Add border color

--- module1-soluction/app.js
+++ module1-soluction/app.js
@@ -12,16 +12,19 @@
             var data = $scope.lunches.split(',');
             if ($scope.lunches.length == 0) {
                 $scope.messaje = 'Please entre data first';
+                $scope.border='borderred';
                 return;
             }
             if (data.length <= 3) {
                 $scope.messaje = 'Enjoy!';
                 $scope.color='green';
+                $scope.border='bordergreen';
             }
             else{
               
                 $scope.messaje = 'Too much!';
                 $scope.color='red';
+                $scope.border='bordergreen';
             }
         }
         //  var lunches=$scope.lunches.split(,);
