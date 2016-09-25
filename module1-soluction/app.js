26e8f3cb052b636e67364c846bc2b0321f2d35ac
MISAEL DIFO CONTRERA <misaedifo@hotmail.com>
2016-09-25 17:09:54 -0400

    add lunchController

--- module1-soluction/app.js
+++ module1-soluction/app.js
@@ -1,3 +1,30 @@
-(function() {
-    'use strict'
+(function () {
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope']
+    function LunchCheckController($scope) {
+        $scope.lunches = '';
+        $scope.messaje = '';
+        $scope.color='';
+
+        $scope.checkIfTooMuch = function () {
+            var data = $scope.lunches.split(',');
+            if ($scope.lunches.length == 0) {
+                $scope.messaje = 'Please entre data first';
+                return;
+            }
+            if (data.length <= 3) {
+                $scope.messaje = 'Enjoy!';
+                $scope.color='green';
+            }
+            else{
+              
+                $scope.messaje = 'Too much!';
+                $scope.color='red';
+            }
+        }
+        //  var lunches=$scope.lunches.split(,);
+
+    }
 })();
