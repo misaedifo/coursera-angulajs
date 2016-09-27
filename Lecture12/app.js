f8d3c869d60cb6506d168fb713bc9ba9dc54e16b
MISAEL DIFO CONTRERA <misaedifo@hotmail.com>
2016-09-27 17:52:04 -0400

    add custom filters

--- Lecture12/app.js
+++ Lecture12/app.js
@@ -1,24 +1,44 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+  angular.module('MsgApp', [])
+    .controller('MsgController', MsgController)
+    .filter('loves', LovesFilter)
+    .filter('truth',TruthFilter);
 
-MsgController.$inject = ['$scope', '$filter'];
-function MsgController($scope, $filter) {
-  $scope.name = "Yaakov";
-  $scope.stateOfBeing = "hungry";
-  $scope.cookieCost = .45;
+  MsgController.$inject = ['$scope', '$filter', 'lovesFilter'];
+  function MsgController($scope, $filter, LovesFilter) {
+    $scope.name = "Yaakov";
+    $scope.stateOfBeing = "hungry";
+    $scope.cookieCost = .45;
 
-  $scope.sayMessage = function () {
-    var msg = "Yaakov likes to eat healthy snacks at night!";
-    var output = $filter('uppercase')(msg);
-    return output;
-  };
+    $scope.sayMessage = function () {
+      var msg = "Yaakov likes to eat healthy snacks at night!";
+      var output = $filter('uppercase')(msg);
+      return output;
+    };
+    $scope.sayLovesMessage = function () {
+      var msg = "Yaakov likes to eat healthy snacks at night!";
+      msg = LovesFilter(msg);
+      return msg;
+    };
+    $scope.feedYaakov = function () {
+      $scope.stateOfBeing = "fed";
+    };
+  }
 
-  $scope.feedYaakov = function () {
-    $scope.stateOfBeing = "fed";
-  };
-}
-
+  function LovesFilter() {
+    return function (input) {
+      input = input || "";
+      input = input.replace("likes", "Love");
+      return input;
+    };
+  }
+  function TruthFilter() {
+    return function (input, target, replace) {
+      input = input || "";
+      input = input.replace(target,replace);
+      return input;
+    };
+  }
 })();
